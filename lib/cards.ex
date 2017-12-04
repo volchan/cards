2e8878d6900b2e81a9e32f2406f3efd85582375b
stephane DE LA TORRE <s.dlt@hotmail.fr>
2017-12-04 18:36:46 +0100

    session 2/20 done

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,6 +1,11 @@
 defmodule Cards do
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["ace", "two", "three", "four", "five", "six", "seven", "height", "nine", "ten", "jack", "queen", "king"]
+    suits = ["spades", "clubs", "hearts", "diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
   end
 
   def shuffle(deck) do
@@ -10,5 +15,28 @@ defmodule Cards do
   def contains?(deck, card) do
     capitalized_card = card |> String.capitalize
     deck |> Enum.member?(capitalized_card)
+  end
+
+  def deal(deck, hand_size) do
+    deck |> Enum.split(hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    filename |> File.write(binary)
+  end
+
+  def load(filename) do
+    # {status, binary} = File.read("lib/db/#{filename}")
+    #
+    # case status do
+    #   :ok -> :erlang.binary_to_term(binary)
+    #   :error -> "This file doesn't exist!"
+    # end
+
+    case File.read("lib/db/#{filename}") do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _} -> "This file doesn't exist!"
+    end
   end
 end
