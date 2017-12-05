4824d68191ba485e3ef2dede7788fd8af76640ce
stephane DE LA TORRE <s.dlt@hotmail.fr>
2017-12-05 12:01:05 +0100

    session 4/32 done

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,22 +1,55 @@
 defmodule Cards do
+  @moduledoc """
+    Provides Methodes for creating and handling a deck of cards.
+  """
+  @doc """
+    Returns a list of strings representing a deck of cards.
+  """
   def create_deck do
     values = ["ace", "two", "three", "four", "five", "six", "seven", "height", "nine", "ten", "jack", "queen", "king"]
     suits = ["spades", "clubs", "hearts", "diamonds"]
 
+    # deck = for suit <- suits  do
+    #   for value <- values  do
+    #     "#{value} of #{suit}"
+    #   end
+    # end
+    # deck |> List.flatten
     for suit <- suits, value <- values do
       "#{value} of #{suit}"
     end
   end
 
+  @doc """
+    Shuffles a deck of cards, randomizing the list past to it.
+  """
   def shuffle(deck) do
     deck |> Enum.shuffle
   end
 
+  @doc """
+    Determines whether a deck contains a given card
+
+  ## Examples
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "ace of spades")
+      true
+  """
   def contains?(deck, card) do
-    capitalized_card = card |> String.capitalize
+    capitalized_card = card |> String.downcase
     deck |> Enum.member?(capitalized_card)
   end
 
+  @doc """
+    Divides a deck into a hand and the reminder of the deck.
+    The `hand_size` argument indicates haw many cards should be in the hand.
+
+  ## Exemples
+      iex> deck = Cards.create_deck
+      iex> {hand, deck_reminder} = Cards.deal(deck, 1)
+      iex> hand
+      ["ace of spades"]
+  """
   def deal(deck, hand_size) do
     deck |> Enum.split(hand_size)
   end
@@ -38,5 +71,11 @@ defmodule Cards do
       {:ok, binary} -> :erlang.binary_to_term(binary)
       {:error, _} -> "This file doesn't exist!"
     end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+      |> Cards.shuffle
+      |> Cards.deal(hand_size)
   end
 end
--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,7 +2,12 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "`create_deck/0` makes 52 cards" do
+    assert length(Cards.create_deck) == 52
+  end
+
+  test "`shuffle/1` shuffles a deck" do
+    deck = Cards.create_deck
+    refute deck == deck |> Cards.shuffle 
   end
 end
